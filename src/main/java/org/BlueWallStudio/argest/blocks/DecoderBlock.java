9079ef4f6fb9a7d31a0f395bc590263e2444ea3d
nijon4rch <blackintensegaming@gmail.com>
2025-08-31 19:00:32 +0300

    Visual changes and fixes
    Improved russian localization for encoder and decoder; added textures, model, rotation and blockstate to decoder; fixed localization for decoder

--- src/main/java/org/BlueWallStudio/argest/blocks/DecoderBlock.java
+++ src/main/java/org/BlueWallStudio/argest/blocks/DecoderBlock.java
@@ -5,9 +5,11 @@ import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
@@ -16,53 +18,63 @@ import org.BlueWallStudio.argest.blocks.entity.DecoderBlockEntity;
 import org.jetbrains.annotations.Nullable;
 
 public class DecoderBlock extends BlockWithEntity {
-    public static final BooleanProperty POWERED = Properties.POWERED;
+	public static final BooleanProperty POWERED = Properties.POWERED;
+	public static final EnumProperty<Direction> FACING = Properties.HORIZONTAL_FACING;
 
-    public DecoderBlock(Settings settings) {
-        super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
-    }
+	public DecoderBlock(Settings settings) {
+		super(settings);
+		this.setDefaultState(this.stateManager.getDefaultState().with(POWERED, false));
+		this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
+	}
 
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(POWERED);
-    }
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(POWERED);
+		builder.add(FACING);
+	}
 
-    @Override
-    protected MapCodec<? extends BlockWithEntity> getCodec() {
-        // ленивое создание codec
-        return createCodec(DecoderBlock::new);
-    }
+	@Override
+	protected MapCodec<? extends BlockWithEntity> getCodec() {
+		// ленивое создание codec
+		return createCodec(DecoderBlock::new);
+	}
 
-    @Override
-    public BlockRenderType getRenderType(BlockState state) {
-        return BlockRenderType.MODEL;
-    }
+	@Override
+	public BlockRenderType getRenderType(BlockState state) {
+		return BlockRenderType.MODEL;
+	}
 
-    @Nullable
-    @Override
-    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new DecoderBlockEntity(pos, state);
-    }
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		// Player-facing side: the block faces opposite the player's look direction
+		return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+	}
 
-    @Nullable
-    @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state,
-                                                                  BlockEntityType<T> type) {
-        return world.isClient ? null : validateTicker(type, ModBlocks.DECODER_BLOCK_ENTITY,
-                DecoderBlockEntity::tick);
-    }
+	@Nullable
+	@Override
+	public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+		return new DecoderBlockEntity(pos, state);
+	}
 
-    @Override
-    public boolean emitsRedstonePower(BlockState state) {
-        return state.get(POWERED);
-    }
+	@Nullable
+	@Override
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state,
+			BlockEntityType<T> type) {
+		return world.isClient ? null
+				: validateTicker(type, ModBlocks.DECODER_BLOCK_ENTITY,
+						DecoderBlockEntity::tick);
+	}
 
-    @Override
-    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
-        if (world.getBlockEntity(pos) instanceof DecoderBlockEntity decoder) {
-            return decoder.getOutputPower(direction.getOpposite());
-        }
-        return 0;
-    }
+	@Override
+	public boolean emitsRedstonePower(BlockState state) {
+		return state.get(POWERED);
+	}
+
+	@Override
+	public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+		if (world.getBlockEntity(pos) instanceof DecoderBlockEntity decoder) {
+			return decoder.getOutputPower(direction.getOpposite());
+		}
+		return 0;
+	}
 }
